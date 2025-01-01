47fa1d39d54fadad7b347e9ec08c643361d2a85b
512Reaper <roanpsaji@gmail.com>
2025-01-01 22:20:58 +0530

    changed app

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
+from app import app
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!-'
 
 
 if __name__ == '__main__':
