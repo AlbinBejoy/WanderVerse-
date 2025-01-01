c95d40cd278b8cac99721063061816df4b13a6fa
512Reaper <roanpsaji@gmail.com>
2025-01-01 22:45:40 +0530

    changed app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+import os
+import dotenv
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///wander.db"
+db = SQLAlchemy(app)
+app.config['secret key'] = os.getenv('SECRET_KEY')
+app.app_context().push()
 
 from app import routes
+from app import models
